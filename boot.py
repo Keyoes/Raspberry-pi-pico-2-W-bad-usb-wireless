01f3c4c722a4003637bb4f1bbf5b2e8ce0d34dbe
Keyoes <93400503+Keyoes@users.noreply.github.com>
2025-05-15 18:49:32 +0300

    Update boot.py

--- boot.py
+++ boot.py
@@ -7,21 +7,17 @@ import board
 import digitalio
 import time
 
-# Configurare LED pentru a indica statusul la inițializare
+
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
-# Aprinde LED-ul pentru a indica că boot.py se execută
 led.value = True
 
-# Activăm modul USB HID pentru a permite Pico să se comporte ca o tastatură
-usb_cdc.disable()  # Dezactivăm consola serială pentru a evita conflicte
-storage.disable_usb_drive()  # Dezactivăm modul de stocare USB
+usb_cdc.disable()  
+storage.disable_usb_drive() 
 
-# Stingem LED-ul pentru a indica finalizarea configurării
 led.value = False
 
-# Un blink scurt pentru a confirma că boot.py s-a executat cu succes
 time.sleep(0.2)
 led.value = True
 time.sleep(0.2)
